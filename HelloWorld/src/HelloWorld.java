20ffb465329f4d9b37588e73d83cddbefbf0a648
Marc Haunschmied <marc.haunschmied@hotmail.com>
2016-04-26 22:14:52 +0200

    first test

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World assafaf");
-
+		
+		
+		for (int i = 0; i < 100; i++){
+			System.out.println(i);
+		}
+		
+		//ok geht
 	}
 
 }
